c29819cc810f7757cb6f0a66f27bdc868833f6c7
Akinnawo3 <Akinnawovictor@gmail.com>
2021-05-11 12:29:41 +0100

    may 11

--- src/Components/Layouts/SideBar.js
+++ src/Components/Layouts/SideBar.js
@@ -3,11 +3,25 @@ import '../../assets/scss/aside.scss'
 import pp from '../../assets/img/user-profile-pic.jpeg'
 
 const SideBar = () => {
+    const menuItems = ["Profile", "Dashboard", "Assets", "Users"]
+
     return (
         <div className="vh-100 bg-white aside">
             <div className="aside-menu">
-                <div className="">
+                <div className="user-avatar">
                     <img src={pp} height={75} className="d-inline-block align-text-top rounded-circle" />
+                </div>
+                <div className="menu-items">
+                    <ul>
+                        {
+                            menuItems.map(
+                                item => <li>
+                                    {item}
+                                </li>
+                            )
+                        }
+
+                    </ul>
                 </div>
             </div>
         </div>
