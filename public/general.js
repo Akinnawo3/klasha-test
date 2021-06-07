8fc96ba1cea011819bb06b10f01836de9b1283c4
unknown <Akinnawovictor@gmail.com>
2021-06-07 03:25:47 +0100

    gg

--- public/general.js
+++ public/general.js
@@ -1,10 +1,12 @@
-document.getElementsByClassName("side-switch")[0].onclick=function(){
-    document.getElementsByClassName("aside")[0].classList.add("expanded");
-}
+// document.getElementsByClassName("side-switch")[0].onclick=function(){
+//     console.log('Expanded')
+//     document.getElementsByClassName("aside")[0].classList.add("expanded");
+// }
 
-document.getElementsByClassName("close-anchor")[0].onclick=function(){
-    document.getElementsByClassName("aside")[0].classList.remove("expanded");
-}
+// document.getElementsByClassName("close-anchor")[0].onclick=function(){
+//     console.log('Closed')
+//     document.getElementsByClassName("aside")[0].classList.remove("expanded");
+// }
 
 
 
--- src/Components/mainNav.js
+++ src/Components/mainNav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 import moment from 'moment'
 import { FaBars, FaBell, FaCircle, FaDotCircle } from 'react-icons/fa'
 import "../assets/scss/mainNav.scss"
@@ -9,10 +9,19 @@ import userImage from '../assets/img/user-profile-pic.jpeg'
 
 
 const MainNav = ({ pageTitle }) => {
+    const sideSwitchRef = useRef();
+    useEffect(() => {
+        const listener = () => {
+            document.querySelector("div.aside").classList.add("expanded");
+        }
+        sideSwitchRef?.current.addEventListener("click", listener);
+        const sRef = sideSwitchRef.current;
+        return () => sRef.removeEventListener('click',listener)
+    }, [])
     return (
         <div className="mt-2 d-flex justify-content-between align-items-center mb-5">
             <div className="d-flex">
-                <div className="me-2 d-md-none side-switch"><FaBars /></div>
+                <div ref={sideSwitchRef} className="me-2 d-md-none side-switch"><FaBars /></div>
                 <div className="left-side d-flex flex-column ">
                     <span className="fw-bold"> {pageTitle}</span>
                     <small className="text-muted ">Today, {moment().format("MMM Do YY")}</small>
